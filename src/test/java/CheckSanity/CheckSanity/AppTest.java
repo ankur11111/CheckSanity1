07a495c42a066b258ddc457f24a61e69762e66c9
ankur <ankur@Ankur.optym.local>
2017-02-16 00:56:13 +0530

    b

--- src/test/java/CheckSanity/CheckSanity/AppTest.java
+++ src/test/java/CheckSanity/CheckSanity/AppTest.java
@@ -36,7 +36,7 @@ public class AppTest
     {
     	
     	System.out.println("Hello 1 in checksanity1");
-        assertTrue( true );
+        assertTrue( false );
       
     }
     public void test2()
